7a806dd9d4e4c9e84662d8ea45ec0be30b7d29c3
omyimplus_oat <omyimplusoat@gmail.com>
2019-01-21 23:23:40 +0700

    update first

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,45 @@
+<div class='nav-main'>
+    <div class='container'>
+        <div class='row py-2 justify-content-between'>
+            <div class='col-3'>
+                <img src="asset/img/logo-doball2day.png" alt="" class='img-fluid'>
+            </div>
+            <div class='col-8 pt-2'>
+                <img src="asset/img/banner/banner-tededlock.jpg" alt="" class='img-fluid'>
+            </div>
+        </div>
+    </div>
+    <div class='navTemp'>
+        <div class='container'>
+            <div class='row'>
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">หน้าแรก</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/live">ดูบอล</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">โปรแกรมบอล</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">วิเคราะห์บอล</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ไฮไลท์ฟุตบอล</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ผลบอล</a>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/liveBall.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+<div class='container py-3'>
+    <div class='row'>
+        <div class='col-12'>
+            <div class='temp-steam'>
+                <img src="asset/img/home/lorem-steam.jpg" alt="" class='img-fluid'>
+                <img src="asset/img/logo-mm88sport-trans.png" alt="" class='logomm88sport'>
+                <img src="asset/img/home/banner-steam.jpg" alt="" class='bannerSteam'>
+            </div>
+        </div>
+        
+    </div>
+</div>
+@endsection
