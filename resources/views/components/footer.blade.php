8dd41aad06714ffe68ca6a44d20ec4b887f4d0f8
omyimplus_oat <omyimplusoat@gmail.com>
2019-01-22 16:04:15 +0700

    again

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class='beforeFooter py-3'>
     <div class='container'>
         <div class='row'>
-            <div class=' col-md-3 d-none d-md-block'>
+            <div class='col-md-3 d-none d-md-block'>
                 <h1>ช่องทางเว็บไซต์</h1>
                 <ul class='list-unstyled'>
                     <li>- <a href="/">หน้าแรก</a></li>
@@ -16,10 +16,10 @@
             <div class='col col-md-9 col-lg-6'>
                 <h1>เกี่ยวกับ <span class='colorYellow'>DOBALL2DAY.COM</span></h1>
                 <div class='row'>
-                    <div class='col-4'>
+                    <div class='col-sm-4'>
                         <img src="asset/img/logo-doball2day-light.png" alt="logo Doball2day" class='img-fluid'>
                     </div>
-                    <div class='col-8 mt-2'>
+                    <div class='col-sm-8 mt-2'>
                         <p class='m-0'>	วิวชนะเลิศสติกเกอร์แป๋ว ซาฟารีอุปนายกอิสรชน ฟอยล์ ฮากกาแดนเซอร์แอ๊บแบ๊วคลับว่ะกุนซือไฮไลต์คอนแทค เซฟตี้ซาร์ดีนเวิลด์อุปนา</p>
                     </div>
                     <div class='col-12 mt-2'>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,26 +44,7 @@
             </div>
         </div>
     </div>
-    <div class='bannerLayout container my-3'>
-        <div class='row'>
-            <div class='col-md-8 pr-md-0 pr-lg-3'>
-                <a href="#">
-                    <img src="asset/img/banner/banner-movie.jpg" alt="" class='img-fluid mt-md-2 mt-lg-3 w-100'>
-                </a>
-                <a href="#">
-                    <img src="asset/img/banner/banner-baccarat.jpg" alt="" class='img-fluid mt-2 mt-md-3 w-100'>
-                </a>
-                <a href="#">
-                    <img src="asset/img/banner/banner-tededstep.jpg" alt="" class='img-fluid mt-2 mt-md-3 w-100'>
-                </a>
-            </div>
-            <div class='col-md-4 mt-3 mt-md-0'>
-                <a href="#">
-                    <img src="asset/img/banner/banner-mm88sport-height.jpg" alt="" class='img-fluid w-100'>
-                </a>
-            </div>
-        </div>
-    </div>
+    @include('components.banner')
     <div class='review container mb-3'>
         <div class='row'>
             <div class='col-lg-7'>
